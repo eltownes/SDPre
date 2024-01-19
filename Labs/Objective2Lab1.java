208ca0eb2e38e17ff0aa5874515204a4354198f7
eltownes <50155313+eltownes@users.noreply.github.com>
2024-01-19 10:15:33 -0500

    finished objective 2

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Objective2Lab1 {
+
+    public static void main(String[] args) {
+        String hello = "Hello World";
+
+        System.out.println(hello);
+    }
+
+}
---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+
+    public static void main(String[] args) {
+        String name = "Eric Townes";
+        int age = 53;
+        String state = "Florida";
+
+        System.out.printf("Name: %s Age: %s State: %s%n", name, age, state);
+    }
+
+}
